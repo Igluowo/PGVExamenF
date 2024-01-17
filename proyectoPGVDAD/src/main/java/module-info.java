8b9d7bbcc66dab0dad43c9fee9062873b2ad484a
erick <elio0eri@gmail.com>
2024-01-17 11:52:44 +0000

    Creando la interfaz de monitoreo

--- proyectoPGVDAD/src/main/java/module-info.java
+++ proyectoPGVDAD/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.proyectopgvdad {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.proyectopgvdad to javafx.fxml;
     exports com.mycompany.proyectopgvdad;
