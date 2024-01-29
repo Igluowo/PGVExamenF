076dc71afe5e719a1981a73fdfb34fe30ba759c0
Erick <elio0eri@gmail.com>
2024-01-29 12:25:11 +0000

    12:26, 29/01/2024. Haciendo conexion con tcp

--- proyectoPGVDAD/src/main/java/module-info.java
+++ proyectoPGVDAD/src/main/java/module-info.java
@@ -2,6 +2,8 @@ module com.mycompany.proyectopgvdad {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires com.github.oshi;
+    requires java.logging;
 
     opens com.mycompany.proyectopgvdad to javafx.fxml;
     exports com.mycompany.proyectopgvdad;
