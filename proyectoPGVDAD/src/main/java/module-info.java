99f2b6a6feeaa6e808677402d9abc0de179e33c4
Erick <118293699+Igluowo@users.noreply.github.com>
2024-01-31 23:23:10 +0000

    Delete proyectoPGVDAD directory

--- proyectoPGVDAD/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.mycompany.proyectopgvdad {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-    requires java.logging;
-
-    opens com.mycompany.proyectopgvdad to javafx.fxml;
-    exports com.mycompany.proyectopgvdad;
-}
